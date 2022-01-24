a01e526260b8a9ae1c851865cf541814a9381c32
eadogan <e.ahmetdogan@gmail.com>
2022-01-24 15:05:32 +0000

    Create skeleton of basic feign client

---
+++ src/main/java/com/feignclient/feignclientproject/FeignClientProjectApplication.java
@@ -0,0 +1,13 @@
+package com.feignclient.feignclientproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeignClientProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeignClientProjectApplication.class, args);
+	}
+
+}
