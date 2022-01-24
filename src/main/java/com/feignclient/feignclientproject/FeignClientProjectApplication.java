8ad8a357c2f37bce04bbaaba3c85fa25c240cfe3
eadogan <e.ahmetdogan@gmail.com>
2022-01-24 15:06:41 +0000

    Enabled FeignClient

--- src/main/java/com/feignclient/feignclientproject/FeignClientProjectApplication.java
+++ src/main/java/com/feignclient/feignclientproject/FeignClientProjectApplication.java
@@ -2,8 +2,10 @@ package com.feignclient.feignclientproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class FeignClientProjectApplication {
 
 	public static void main(String[] args) {
